3ef4dc3aa1b9d7cb00949a080ee184918cb937ad
goeunlee <goeun3501@gmail.com>
2024-09-12 15:41:00 +0900

    [style]styled component 분리

--- src/app/Student/page.tsx
+++ src/app/Student/page.tsx
@@ -1,215 +1,36 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import styled from "styled-components";
 import { getStudentData } from "../api/api";
 import { ReportData } from "../type/commonApi";
-import Link_Icon from "../icons/link.svg";
 import Check_Icon from "../icons/check.svg";
 import Filter_Icon from "../icons/filter.svg";
+import {
+	Container,
+	GridContainer,
+	FlexBox,
+	Label,
+	Value,
+	ActivityContainer,
+	NumberBox,
+	Visit_Icon,
+	PrimaryPCButton,
+	HorizontalDivider,
+	GridWrapper,
+	ActivityBox,
+	Filter,
+	Check,
+	PrimaryMobileButton,
+	DarkMobileButton,
+	Achievement,
+	AchievementColumn,
+	AchievementLabel,
+	AchievementsContainer,
+	Divider,
+	AchievementValue,
+} from "../component/common/pageCommon";
+import { styled } from "styled-components";
 
-const theme = {
-	colors: {
-		text: "#000",
-		border: "#e6e9ec",
-		background: "#fff",
-		highlight: "rgba(151, 71, 255, 0.2)",
-		divider: "#b3b3b3",
-		violet: "#7a40f2",
-		main_dark: "#3B3A48",
-	},
-	fontSize: { desktop: "1.125rem", mobile: "1rem" },
-	borderRadius: "10px",
-	padding: "20px",
-	gap: "40px",
-};
-
-const Container = styled.div`
-	padding: ${theme.padding};
-`;
-const GridContainer = styled.div`
-	display: grid;
-	grid-template-columns: repeat(2, 1fr);
-	grid-gap: ${theme.gap};
-	margin: 40px 0;
-	@media (max-width: 840px) {
-		grid-template-columns: 1fr;
-	}
-`;
-const FlexBox = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	padding: 10px 0;
-`;
-
-const Check = styled(Check_Icon)`
-	@media (min-width: 840px) {
-		display: none;
-	}
-`;
-const Filter = styled(Filter_Icon)`
-	@media (min-width: 840px) {
-		display: none;
-	}
-`;
-type BtnTheme = "dark" | "primary" | "primary_active";
-
-interface ButtonProps {
-	btnTheme: BtnTheme;
-}
-const Button = styled.button<ButtonProps>`
-	box-sizing: border-box;
-	width: 147px;
-	height: 54px;
-	font-size: 1.125rem;
-	font-weight: bold;
-	background: ${({ btnTheme }) =>
-		btnTheme === "primary_active" ? theme.colors.background : btnTheme === "dark" ? theme.colors.main_dark : theme.colors.violet};
-	border: 1px solid ${({ btnTheme }) => (btnTheme === "primary" || btnTheme === "primary_active" ? theme.colors.violet : theme.colors.main_dark)};
-	border-radius: ${theme.borderRadius};
-	color: ${({ btnTheme }) => (btnTheme === "primary_active" ? theme.colors.violet : theme.colors.background)};
-	transition: background-color 0.3s ease, color 0.3s ease;
-	cursor: pointer;
-
-	&:hover {
-		background: ${({ btnTheme }) => (btnTheme === "primary_active" ? theme.colors.violet : theme.colors.background)};
-		color: ${({ btnTheme }) => (btnTheme === "dark" ? theme.colors.main_dark : btnTheme === "primary_active" ? theme.colors.background : theme.colors.violet)};
-		border-color: ${({ btnTheme }) => (btnTheme === "dark" ? theme.colors.main_dark : theme.colors.violet)};
-	}
-`;
-
-const PrimaryPCButton = styled(Button)`
-	@media (min-width: 840px) {
-		display: block;
-	}
-	@media (max-width: 840px) {
-		display: none;
-	}
-`;
-const PrimaryMobileButton = styled(Button)`
-	@media (min-width: 840px) {
-		display: none;
-	}
-	@media (max-width: 840px) {
-		display: block;
-	}
-`;
-const DarkMobileButton = styled(Button)`
-	@media (min-width: 840px) {
-		display: none;
-	}
-	@media (max-width: 840px) {
-		display: block;
-	}
-`;
-
-const Label = styled.span`
-	font-weight: bold;
-	font-size: ${theme.fontSize.desktop};
-	color: ${theme.colors.text};
-	@media (max-width: 840px) {
-		font-size: ${theme.fontSize.mobile};
-	}
-`;
-
-const Value = styled.span`
-	font-size: ${theme.fontSize.desktop};
-	color: ${theme.colors.text};
-	@media (max-width: 840px) {
-		font-size: ${theme.fontSize.mobile};
-	}
-`;
-
-const AchievementsContainer = styled.div`
-	display: flex;
-	justify-content: space-between;
-	margin-top: 10px;
-	@media (max-width: 840px) {
-		display: block;
-	}
-	@media (min-width: 840px) {
-		background-color: ${theme.colors.highlight};
-		border-radius: ${theme.borderRadius};
-		padding: 10px 40px;
-	}
-`;
-
-const ActivityContainer = styled.div`
-	background: ${theme.colors.background};
-	border: 1px solid ${theme.colors.border};
-	border-radius: ${theme.borderRadius};
-	padding: 30px 50px;
-	margin: 10px 0;
-
-	@media (max-width: 840px) {
-		padding: 20px;
-
-		.title {
-			font-size: 1.125rem;
-		}
-	}
-
-	.title {
-		display: flex;
-		font-size: 1.25rem;
-		font-weight: bold;
-		gap: 10px;
-		margin: 0 0 10px 0;
-		color: ${theme.colors.text};
-	}
-
-	.label {
-		font-weight: bold;
-		font-size: 1.125rem;
-		color: ${theme.colors.text};
-	}
-
-	.value {
-		font-weight: normal;
-		font-size: 1.125rem;
-		color: ${theme.colors.text};
-	}
-`;
-
-const ActivityBox = styled(FlexBox)`
-	justify-content: flex-start;
-	gap: 5px;
-`;
-
-const NumberBox = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 27px;
-	height: 28px;
-	min-width: 27px;
-	min-height: 28px;
-	background: ${theme.colors.violet};
-	border-radius: 3px;
-	color: ${theme.colors.background};
-`;
-
-const Visit_Icon = styled(Link_Icon)`
-	@media (max-width: 840px) {
-		display: none;
-	}
-`;
-
-const GridWrapper = styled.div`
-	display: grid;
-	grid-template-columns: repeat(1, 1fr);
-	gap: 10px;
-
-	@media (min-width: 840px) {
-		grid-template-columns: repeat(3, 1fr);
-	}
-`;
-const HorizontalDivider = styled.div`
-	width: 100%;
-	border: 2px dashed ${theme.colors.border};
-	margin: 20px 0;
-`;
 export default function StudentPage() {
 	const [data, setData] = useState<ReportData | null>(null);
 	const [error, setError] = useState<Error | null>(null);
@@ -257,7 +78,43 @@ export default function StudentPage() {
 					</FlexBox>
 				))}
 			</GridContainer>
+			<Label>Total : 2</Label>
+			<AchievementsContainer>
+				<AchievementColumn>
+					<Achievement>
+						<AchievementLabel>✍️ Writing Competition</AchievementLabel>
+						<AchievementValue>-</AchievementValue>
+					</Achievement>
+					<Achievement>
+						<AchievementLabel>🏆 Competition</AchievementLabel>
+						<AchievementValue>-</AchievementValue>
+					</Achievement>
+					<Divider />
+				</AchievementColumn>
 
+				<AchievementColumn>
+					<Achievement>
+						<AchievementLabel>🏫 Pre-College</AchievementLabel>
+						<AchievementValue>-</AchievementValue>
+					</Achievement>
+					<Achievement>
+						<AchievementLabel>💼 Internship</AchievementLabel>
+						<AchievementValue>-</AchievementValue>
+					</Achievement>
+					<Divider />
+				</AchievementColumn>
+
+				<AchievementColumn>
+					<Achievement>
+						<AchievementLabel>⛑️ Volunteer</AchievementLabel>
+						<AchievementValue>-</AchievementValue>
+					</Achievement>
+					<Achievement>
+						<AchievementLabel>🔎 Research</AchievementLabel>
+						<AchievementValue>-</AchievementValue>
+					</Achievement>
+				</AchievementColumn>
+			</AchievementsContainer>
 			{activity.length > 0 ? (
 				activity.map((item, index) => (
 					<ActivityContainer key={index}>
---
+++ src/app/component/common/pageCommon.tsx
@@ -0,0 +1,264 @@
+import styled from "styled-components";
+import Link_Icon from "../../icons/link.svg";
+import Check_Icon from "../../icons/check.svg";
+import Filter_Icon from "../../icons/filter.svg";
+
+export const theme = {
+	colors: {
+		text: "#000",
+		border: "#e6e9ec",
+		background: "#ffffff",
+		highlight: "rgba(151, 71, 255, 0.2)",
+		divider: "#b3b3b3",
+		violet: "#7a40f2",
+		main_dark: "#3B3A48",
+	},
+	fontSize: { desktop: "1.125rem", mobile: "1rem" },
+	borderRadius: "10px",
+	padding: "20px",
+	gap: "40px",
+};
+
+export const Container = styled.div`
+	padding: ${theme.padding};
+`;
+export const GridContainer = styled.div`
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	grid-gap: ${theme.gap};
+	margin: 40px 0;
+	@media (max-width: 840px) {
+		grid-template-columns: 1fr;
+		background-color: ${theme.colors.background};
+		padding: 20px;
+		border-radius: 10px;
+	}
+`;
+export const FlexBox = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 10px 0;
+`;
+
+export const Check = styled(Check_Icon)`
+	@media (min-width: 840px) {
+		display: none;
+	}
+`;
+export const Filter = styled(Filter_Icon)`
+	@media (min-width: 840px) {
+		display: none;
+	}
+`;
+type BtnTheme = "dark" | "primary" | "primary_active";
+
+interface ButtonProps {
+	btnTheme: BtnTheme;
+}
+export const Button = styled.button<ButtonProps>`
+	box-sizing: border-box;
+	width: 147px;
+	height: 54px;
+	font-size: 1.125rem;
+	font-weight: bold;
+	background: ${({ btnTheme }) =>
+		btnTheme === "primary_active" ? theme.colors.background : btnTheme === "dark" ? theme.colors.main_dark : theme.colors.violet};
+	border: 1px solid ${({ btnTheme }) => (btnTheme === "primary" || btnTheme === "primary_active" ? theme.colors.violet : theme.colors.main_dark)};
+	border-radius: ${theme.borderRadius};
+	color: ${({ btnTheme }) => (btnTheme === "primary_active" ? theme.colors.violet : theme.colors.background)};
+	transition: background-color 0.3s ease, color 0.3s ease;
+	cursor: pointer;
+
+	&:hover {
+		background: ${({ btnTheme }) => (btnTheme === "primary_active" ? theme.colors.violet : theme.colors.background)};
+		color: ${({ btnTheme }) => (btnTheme === "dark" ? theme.colors.main_dark : btnTheme === "primary_active" ? theme.colors.background : theme.colors.violet)};
+		border-color: ${({ btnTheme }) => (btnTheme === "dark" ? theme.colors.main_dark : theme.colors.violet)};
+	}
+`;
+
+export const PrimaryPCButton = styled(Button)`
+	@media (min-width: 840px) {
+		display: block;
+	}
+	@media (max-width: 840px) {
+		display: none;
+	}
+`;
+export const PrimaryMobileButton = styled(Button)`
+	@media (min-width: 840px) {
+		display: none;
+	}
+	@media (max-width: 840px) {
+		display: block;
+	}
+`;
+export const DarkMobileButton = styled(Button)`
+	@media (min-width: 840px) {
+		display: none;
+	}
+	@media (max-width: 840px) {
+		display: block;
+	}
+`;
+
+export const Label = styled.span`
+	font-weight: bold;
+	font-size: ${theme.fontSize.desktop};
+	color: ${theme.colors.text};
+	@media (max-width: 840px) {
+		font-size: ${theme.fontSize.mobile};
+	}
+`;
+
+export const Value = styled.span`
+	font-size: ${theme.fontSize.desktop};
+	color: ${theme.colors.text};
+	@media (max-width: 840px) {
+		font-size: ${theme.fontSize.mobile};
+	}
+`;
+
+export const ActivityContainer = styled.div`
+	background: ${theme.colors.background};
+	border: 1px solid ${theme.colors.border};
+	border-radius: ${theme.borderRadius};
+	padding: 30px 50px;
+	margin: 10px 0;
+
+	@media (max-width: 840px) {
+		padding: 20px;
+
+		.title {
+			font-size: 1.125rem;
+		}
+	}
+
+	.title {
+		display: flex;
+		font-size: 1.25rem;
+		font-weight: bold;
+		gap: 10px;
+		margin: 0 0 10px 0;
+		color: ${theme.colors.text};
+	}
+
+	.label {
+		font-weight: bold;
+		font-size: 1.125rem;
+		color: ${theme.colors.text};
+	}
+
+	.value {
+		font-weight: normal;
+		font-size: 1.125rem;
+		color: ${theme.colors.text};
+	}
+`;
+
+export const ActivityBox = styled(FlexBox)`
+	justify-content: flex-start;
+	gap: 5px;
+`;
+
+export const NumberBox = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 27px;
+	height: 28px;
+	min-width: 27px;
+	min-height: 28px;
+	background: ${theme.colors.violet};
+	border-radius: 3px;
+	color: ${theme.colors.background};
+`;
+
+export const Visit_Icon = styled(Link_Icon)`
+	@media (max-width: 840px) {
+		display: none;
+	}
+`;
+
+export const GridWrapper = styled.div`
+	display: grid;
+	grid-template-columns: repeat(1, 1fr);
+	gap: 10px;
+
+	@media (min-width: 840px) {
+		grid-template-columns: repeat(3, 1fr);
+	}
+`;
+export const HorizontalDivider = styled.div`
+	width: 100%;
+	border: 2px dashed ${theme.colors.border};
+	margin: 20px 0;
+`;
+
+export const AchievementsContainer = styled.div`
+	margin-top: 40px;
+	background-color: transparent;
+	border-radius: 10px;
+	padding: 20px;
+	color: white;
+	display: flex;
+	justify-content: space-between;
+
+	@media (max-width: 840px) {
+		display: block;
+		padding: 0;
+	}
+	@media (min-width: 840px) {
+		background-color: rgba(151, 71, 255, 0.2);
+	}
+`;
+
+export const AchievementColumn = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 100%;
+	padding: 0 20px;
+	position: relative;
+
+	@media (max-width: 840px) {
+		padding: 0;
+	}
+`;
+
+export const Achievement = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+	background-color: transparent;
+	padding: 10px 0;
+
+	@media (max-width: 840px) {
+		background-color: rgba(151, 71, 255, 0.2);
+		border-radius: 10px;
+		padding: 20px;
+		margin-bottom: 10px;
+	}
+`;
+
+export const AchievementLabel = styled(Label)`
+	flex: 1;
+`;
+
+export const AchievementValue = styled(Value)`
+	text-align: right;
+	flex-shrink: 0;
+`;
+
+export const Divider = styled.div`
+	width: 1px;
+	background-color: #b3b3b3;
+	position: absolute;
+	height: 100%;
+	right: 0;
+
+	@media (max-width: 840px) {
+		display: none;
+	}
+`;
